b14f23e53e80ff88285166cdbdd17fadc8709a76
stivcode21 <stivenelcodificador@gmail.com>
2025-11-12 23:24:00 -0500

    refactor: component welcome It now has less code because it uses the template

--- Frontend/src/pages/welcome/Welcome.jsx
+++ Frontend/src/pages/welcome/Welcome.jsx
@@ -1,58 +1,60 @@
-import Logo from "@/components/atoms/logo/Logo";
 import styles from "./Welcome.module.css";
 import { InfoIcon, Lock } from "lucide-react";
+import Logo from "@/components/atoms/logo/Logo";
+import WelcomeLayout from "../../components/templates/welcomeLayout/WelcomeLayout";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
+  const [isWelcome, setIsWelcome] = useState(true);
+  const navigate = useNavigate();
+
+  const toggleSection = (isWelcome) => {
+    setIsWelcome(!isWelcome);
+    navigate("/login");
+  };
+
   return (
-    <main className={styles.container}>
-      <figure className={styles.decorations} aria-hidden="true">
-        <img src="/public/shappe.png" className={styles.shappe} alt="" />
-        <img src="/public/marca-agua.png" className={styles.marcaAgua} alt="" />
-      </figure>
+    <WelcomeLayout isWelcome={isWelcome}>
+      <header className={styles.header}>
+        <Logo />
+      </header>
 
-      <section className={styles.flexbox} aria-labelledby="welcome-heading">
-        <article className={styles.modal}>
-          <header className={styles.header}>
-            <Logo />
-          </header>
-
-          <section aria-label="Beneficios principales">
-            <ul className={styles.list}>
-              <li className={styles.item}>
-                Gestión industrial de tu finca en un solo lugar
-              </li>
-              <li className={styles.item}>
-                Supervisión eficiente de todo tu personal
-              </li>
-              <li className={styles.item}>
-                Control financiero claro y confiable
-              </li>
-            </ul>
-          </section>
-
-          <nav className={styles.boxButtons} aria-label="Acciones principales">
-            <button type="button" className={styles.button}>
-              <span aria-hidden="true">
-                <Lock />
-              </span>
-              iniciar sesión
-            </button>
-            <button type="button" className={styles.outline}>
-              <span aria-hidden="true">
-                <InfoIcon />
-              </span>
-              info sistema
-            </button>
-          </nav>
-
-          <footer className={styles.footer}>
-            Accede como dueño o administrador según tus credenciales
-          </footer>
-        </article>
-
-        <div className={styles.emty}></div>
+      <section aria-label="Beneficios principales">
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            Gestión industrial de tu finca en un solo lugar
+          </li>
+          <li className={styles.item}>
+            Supervisión eficiente de todo tu personal
+          </li>
+          <li className={styles.item}>Control financiero claro y confiable</li>
+        </ul>
       </section>
-    </main>
+
+      <nav className={styles.boxButtons} aria-label="Acciones principales">
+        <button
+          type="button"
+          className={styles.button}
+          onClick={() => toggleSection(isWelcome)}
+        >
+          <span aria-hidden="true">
+            <Lock />
+          </span>
+          iniciar sesión
+        </button>
+        <button type="button" className={styles.outline}>
+          <span aria-hidden="true">
+            <InfoIcon />
+          </span>
+          info sistema
+        </button>
+      </nav>
+
+      <footer className={styles.footer}>
+        Accede como dueño o administrador según tus credenciales
+      </footer>
+    </WelcomeLayout>
   );
 };
 
