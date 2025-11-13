57b198972f62f53e0a23f2592d1042765348b9f8
stivcode21 <stivenelcodificador@gmail.com>
2025-11-12 23:26:45 -0500

    feat: import component y create new route

--- Frontend/src/routing/AppRouter.jsx
+++ Frontend/src/routing/AppRouter.jsx
@@ -1,10 +1,12 @@
 import Welcome from "@/pages/welcome/Welcome";
 import { Routes, Route } from "react-router-dom";
+import Login from "@/pages/login/Login";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
