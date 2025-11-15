50181693b76c52c704032bedf4328425bf3befd4
stivcode21 <stivenelcodificador@gmail.com>
2025-11-14 23:21:21 -0500

    Almost complete functionality and basic design, too import Infomodal

--- Frontend/src/pages/welcome/Welcome.jsx
+++ Frontend/src/pages/welcome/Welcome.jsx
@@ -1,13 +1,15 @@
 import styles from "./Welcome.module.css";
 import { InfoIcon, Lock } from "lucide-react";
 import Logo from "@/components/atoms/logo/Logo";
-import WelcomeLayout from "../../components/templates/welcomeLayout/WelcomeLayout";
+import WelcomeLayout from "@/components/templates/welcomeLayout/WelcomeLayout";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Button from "../../components/templates/button/Button";
+import Button from "@/components/templates/button/Button";
+import InfoModal from "@/components/organism/infoModal/InfoModal";
 
 const Welcome = () => {
   const [isWelcome, setIsWelcome] = useState(true);
+  const [isOpenModal, setIsOpenModal] = useState(false);
   const navigate = useNavigate();
 
   const toggleSection = (isWelcome) => {
@@ -16,42 +18,47 @@ const Welcome = () => {
   };
 
   return (
-    <WelcomeLayout isWelcome={isWelcome}>
-      <header className={styles.header}>
-        <Logo />
-      </header>
+    <>
+      <WelcomeLayout isWelcome={isWelcome}>
+        <header className={styles.header}>
+          <Logo />
+        </header>
 
-      <section aria-label="Beneficios principales">
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            Gestión industrial de tu finca en un solo lugar
-          </li>
-          <li className={styles.item}>
-            Supervisión eficiente de todo tu personal
-          </li>
-          <li className={styles.item}>Control financiero claro y confiable</li>
-        </ul>
-      </section>
+        <section aria-label="Beneficios principales">
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              Gestión industrial de tu finca en un solo lugar
+            </li>
+            <li className={styles.item}>
+              Supervisión eficiente de todo tu personal
+            </li>
+            <li className={styles.item}>
+              Control financiero claro y confiable
+            </li>
+          </ul>
+        </section>
 
-      <nav className={styles.boxButtons} aria-label="Acciones principales">
-        <Button onClick={() => toggleSection(isWelcome)}>
-          <span aria-hidden="true">
-            <Lock />
-          </span>
-          iniciar sesión
-        </Button>
-        <Button type="secondary">
-          <span aria-hidden="true">
-            <InfoIcon />
-          </span>
-          info sistema
-        </Button>
-      </nav>
+        <nav className={styles.boxButtons} aria-label="Acciones principales">
+          <Button onClick={() => toggleSection(isWelcome)}>
+            <span aria-hidden="true">
+              <Lock />
+            </span>
+            iniciar sesión
+          </Button>
+          <Button type="secondary" onClick={() => setIsOpenModal(!isOpenModal)}>
+            <span aria-hidden="true">
+              <InfoIcon />
+            </span>
+            info sistema
+          </Button>
+        </nav>
 
-      <footer className={styles.footer}>
-        Accede como dueño o administrador según tus credenciales
-      </footer>
-    </WelcomeLayout>
+        <footer className={styles.footer}>
+          Accede como dueño o administrador según tus credenciales
+        </footer>
+      </WelcomeLayout>
+      <InfoModal stateCurrent={isOpenModal} setState={setIsOpenModal} />
+    </>
   );
 };
 
