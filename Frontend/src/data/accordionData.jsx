5587be116cbf8b1bb88c9a01636667dcb9a1ec4c
stivcode21 <stivenelcodificador@gmail.com>
2025-11-20 19:11:25 -0500

    feat: created data for indo in features section

--- Frontend/src/data/accordionData.jsx
+++ Frontend/src/data/accordionData.jsx
@@ -1,11 +1,22 @@
-import { Building2, ChartPie, MessageCircleQuestionMark } from "lucide-react";
-import panels from "@/assets/panels.svg";
-import template from "@/assets/template.svg";
-import tree from "@/assets/tree.svg";
 import {
   AboutUs,
   Features,
 } from "../components/organism/sectionsInfo/SectionsInfo";
+import {
+  Building2,
+  ChartNoAxesCombined,
+  ChartPie,
+  LayoutListIcon,
+  MessageCircleQuestionMark,
+  Users,
+} from "lucide-react";
+import panels from "@/assets/icons/panels.svg";
+import template from "@/assets/icons/template.svg";
+import tree from "@/assets/icons/tree.svg";
+
+import cow from "@/assets/icons/cow.svg";
+import pig from "@/assets/icons/pig.svg";
+import notes from "@/assets/icons/notes.svg";
 
 export const options = [
   {
@@ -42,5 +53,38 @@ export const aboutInfo = [
     title: "Evolución e innovación segura",
     shortDescription:
       "versión 1.0 en constante mejora y expansión de funciones",
+  },
+];
+
+export const featureInfo = [
+  {
+    svg: <ChartNoAxesCombined />,
+    title: "Informes y analisis",
+    description: "Gestiona toda tu finca desde un solo lugar",
+  },
+  {
+    svg: <LayoutListIcon />,
+    title: "Inventario de insumos",
+    description: "Visualización de reporte y descarga de reportes",
+  },
+  {
+    svg: <Users />,
+    title: "Trabajadores",
+    description: "Registro de pagos, control y gestión de trabajadores",
+  },
+  {
+    svg: cow,
+    title: "Ganaderia",
+    description: "Registro de nacimientos, vacunacion, ventas, compras etc.",
+  },
+  {
+    svg: pig,
+    title: "Porcicultura",
+    description: "Registro de nacimientos, vacunacion, ventas, compras etc.",
+  },
+  {
+    svg: notes,
+    title: "Mantenimiento",
+    description: "Control de gastos y registro de mantenimiento",
   },
 ];
