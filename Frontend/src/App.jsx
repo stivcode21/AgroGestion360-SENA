729306df4803885a71d8909ef0c07843344bed16
stivcode21 <stivenelcodificador@gmail.com>
2025-11-22 10:30:49 -0500

    feat; Import the theme and settings into the app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routing/AppRouter";
+import { useThemeStore } from "./store/ThemeStore";
+import { useEffect } from "react";
 
 function App() {
+  const { isLightMode } = useThemeStore();
+
+  useEffect(() => {
+    document.body.className = isLightMode ? "light-mode" : "dark-mode";
+  }, [isLightMode]);
+
   return (
     <BrowserRouter>
       <AppRouter />
