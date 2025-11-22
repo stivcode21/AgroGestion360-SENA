fb64cf57f5885a0a5b6c6a8fcdc7f3b0476d5c1c
stivcode21 <stivenelcodificador@gmail.com>
2025-11-22 10:51:51 -0500

    feat: import component for testing

--- Frontend/src/routing/AppRouter.jsx
+++ Frontend/src/routing/AppRouter.jsx
@@ -1,12 +1,14 @@
 import Welcome from "@/pages/welcome/Welcome";
 import { Routes, Route } from "react-router-dom";
 import Login from "@/pages/login/Login";
+import MainLayout from "../components/templates/mainLayout/MainLayout";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/test" element={<MainLayout />} />
     </Routes>
   );
 };
