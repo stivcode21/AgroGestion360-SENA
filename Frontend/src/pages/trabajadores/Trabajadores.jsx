c76114a97499ff31ba567519e2ab08ad8ce49181
stivcode21 <stivenelcodificador@gmail.com>
2025-11-24 12:39:38 -0500

    feat: Create an trabajadores page with a default design

---
+++ Frontend/src/pages/trabajadores/Trabajadores.jsx
@@ -0,0 +1,17 @@
+import MainLayout from "@/components/templates/mainLayout/MainLayout";
+import styles from "./Trabajadores.module.css";
+
+const Trabajadores = () => {
+  return (
+    <MainLayout>
+      <section className={styles.page}>
+        <h1 className={styles.title}>Trabajadores</h1>
+        <p className={styles.description}>
+          Seccion Trabajadores pendiente por contenido.
+        </p>
+      </section>
+    </MainLayout>
+  );
+};
+
+export default Trabajadores;
