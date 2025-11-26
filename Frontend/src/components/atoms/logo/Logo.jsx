fcdade1d652fb4383cddbb7c3fd1b97da8730f9d
stivcode21 <stivenelcodificador@gmail.com>
2025-11-25 22:24:14 -0500

    feat: all component reutility

--- Frontend/src/components/atoms/logo/Logo.jsx
+++ Frontend/src/components/atoms/logo/Logo.jsx
@@ -1,14 +1,30 @@
 import styles from "./Logo.module.css";
 
-const Logo = () => {
+const Logo = ({ size, collapsed }) => {
+  const small = size === "small";
+
   return (
-    <a href="/" className={styles.container}>
-      <img src="/public/logo.svg" alt="Parkify Logo" className={styles.logo} />
-      <h1 className={styles.title}>
-        AgroGestion<span className={styles.strong}>360</span>
-      </h1>
-      <div className={styles.line}></div>
-    </a>
+    <header className={styles.container}>
+      {collapsed ? (
+        <img
+          src="/logoSmall.svg"
+          alt="Parkify Logo"
+          className={`${styles.logo} ${small && styles.logoSmall}`}
+        />
+      ) : (
+        <>
+          <img
+            src="/logo.svg"
+            alt="Parkify Logo"
+            className={`${styles.logo} ${small && styles.logoSmall2}`}
+          />
+          <h1 className={`${styles.title} ${small && styles.titleSmall}`}>
+            AgroGestion<span className={styles.strong}>360</span>
+          </h1>
+          <div className={`${styles.line} ${small && styles.line2}`}></div>
+        </>
+      )}
+    </header>
   );
 };
 
