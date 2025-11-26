170cb35b840a316c79a2a7d34bfaeea0655365fd
stivcode21 <stivenelcodificador@gmail.com>
2025-11-25 22:28:47 -0500

    fix: small changes in login

--- Frontend/src/pages/login/Login.jsx
+++ Frontend/src/pages/login/Login.jsx
@@ -1,8 +1,8 @@
-import WelcomeLayout from "../../components/templates/welcomeLayout/WelcomeLayout";
+import WelcomeLayout from "@/components/templates/welcomeLayout/WelcomeLayout";
 import Logo from "@/components/atoms/logo/Logo";
 import styles from "./Login.module.css";
-import { ArrowRight } from "lucide-react";
-import Button from "../../components/templates/button/Button";
+import { ArrowRight, Lock, User } from "lucide-react";
+import Button from "@/components/templates/button/Button";
 
 const Login = () => {
   return (
@@ -14,12 +14,18 @@ const Login = () => {
         <h1 className={styles.title}>INICIAR SESION</h1>
         <div className={styles.containerInput}>
           <label htmlFor="Usuario" className={styles.label}>
+            <i className={styles.icon}>
+              <User />
+            </i>
             Usuario:
           </label>
           <input type="text" className={styles.input} />
         </div>
         <div className={styles.containerInput}>
           <label htmlFor="Usuario" className={styles.label}>
+            <i className={styles.icon}>
+              <Lock />
+            </i>
             Contraseña:
           </label>
           <input type="password" className={styles.input} />
