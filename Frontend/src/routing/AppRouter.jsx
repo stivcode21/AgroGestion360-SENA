90c061eb7b8056c07a70848a41827fa2ec30bb5b
stivcode21 <stivenelcodificador@gmail.com>
2025-11-25 22:40:44 -0500

    feat: import  new route

--- Frontend/src/routing/AppRouter.jsx
+++ Frontend/src/routing/AppRouter.jsx
@@ -1,14 +1,26 @@
-import Welcome from "@/pages/welcome/Welcome";
 import { Routes, Route } from "react-router-dom";
+import Welcome from "@/pages/welcome/Welcome";
 import Login from "@/pages/login/Login";
-import MainLayout from "../components/templates/mainLayout/MainLayout";
+import Dashboard from "@/pages/dashboard/Dashboard";
+import Inventario from "@/pages/inventario/Inventario";
+import Trabajadores from "@/pages/trabajadores/Trabajadores";
+import Actividades from "@/pages/actividades/Actividades";
+import Ganaderia from "@/pages/ganaderia/Ganaderia";
+import Porcicultura from "@/pages/porcicultura/Porcicultura";
+import Reportes from "@/pages/reportes/Reportes";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/test" element={<MainLayout />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/inventario" element={<Inventario />} />
+      <Route path="/reportes" element={<Reportes />} />
+      <Route path="/trabajadores" element={<Trabajadores />} />
+      <Route path="/actividades" element={<Actividades />} />
+      <Route path="/ganaderia" element={<Ganaderia />} />
+      <Route path="/porcicultura" element={<Porcicultura />} />
     </Routes>
   );
 };
