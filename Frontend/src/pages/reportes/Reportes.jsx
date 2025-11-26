05df24f2a4c96a424c4bb60d97aa20fbf2b64b99
stivcode21 <stivenelcodificador@gmail.com>
2025-11-25 22:31:59 -0500

    feat: new page Reportes crate

---
+++ Frontend/src/pages/reportes/Reportes.jsx
@@ -0,0 +1,14 @@
+import styles from "./Reportes.module.css";
+import MainLayout from "@/components/templates/mainLayout/MainLayout";
+
+const Reportes = () => {
+  return (
+    <MainLayout>
+      <section className={styles.page}>
+        <h1>Reportes</h1>
+      </section>
+    </MainLayout>
+  );
+};
+
+export default Reportes;
