d60c224631a140d1a0c38216e11977bbc3e98bdf
stivcode21 <stivenelcodificador@gmail.com>
2025-11-26 21:31:23 -0500

    feat: now dashboard use map for linkCards

--- Frontend/src/pages/dashboard/Dashboard.jsx
+++ Frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,14 +1,34 @@
 import MainLayout from "@/components/templates/mainLayout/MainLayout";
 import styles from "./Dashboard.module.css";
+import Linkcard from "@/components/molecules/linkCard/LinkCard";
+import { sidebarData } from "@/data/sidebarData";
+import { useSidebarStore } from "@/store/sidebarStore";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const { setCurrentSection } = useSidebarStore();
+
+  const handleSection = (path) => {
+    setCurrentSection(path);
+    navigate(path);
+  };
   return (
     <MainLayout>
       <section className={styles.page}>
-        <h1 className={styles.title}>Dashboard</h1>
-        <p className={styles.description}>
-          Seccion Dashboard pendiente por contenido.
-        </p>
+        <article className={styles.content}>
+          {sidebarData.slice(1, 7).map((item, i) => {
+            return (
+              <Linkcard
+                key={i}
+                icon={item.icon}
+                title={item.title}
+                description={item.description}
+                onClick={() => handleSection(item.path)}
+              />
+            );
+          })}
+        </article>
       </section>
     </MainLayout>
   );
