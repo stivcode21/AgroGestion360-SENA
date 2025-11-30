afdc9dc6b49396daf53eef952a61591f30f1d9e5
stivcode21 <stivenelcodificador@gmail.com>
2025-11-29 21:25:13 -0500

    feat: New global status to know if we are on desktop

--- Frontend/src/store/sidebarStore.js
+++ Frontend/src/store/sidebarStore.js
@@ -1,10 +1,14 @@
 import { create } from "zustand";
 
+const initialIsDesktop = window.innerWidth > 768;
+
 export const useSidebarStore = create((set) => ({
   currentSection: "/dashboard",
   isCollapsed: false,
+  isDesktop: initialIsDesktop,
   setCurrentSection: (section) => set({ currentSection: section }),
   setCollapsed: (collapsed) => set({ isCollapsed: collapsed }),
+  setIsDesktop: (value) => set({ isDesktop: value }),
   toggleCollapsed: () =>
     set((state) => ({
       isCollapsed: !state.isCollapsed,
