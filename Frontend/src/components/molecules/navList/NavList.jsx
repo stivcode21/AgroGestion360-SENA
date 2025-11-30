28ebcfae8853a992e9bc172252cdb5c64c42b2fe
stivcode21 <stivenelcodificador@gmail.com>
2025-11-29 21:21:49 -0500

    feat: new component navList for options navbar

---
+++ Frontend/src/components/molecules/navList/NavList.jsx
@@ -0,0 +1,33 @@
+import { sidebarData } from "@/data/sidebarData";
+import { useSidebarStore } from "@/store/sidebarStore";
+import { useNavigate } from "react-router-dom";
+import styles from "./NavList.module.css";
+
+const NavList = () => {
+  const { currentSection, setCurrentSection, isCollapsed } = useSidebarStore();
+  const navigate = useNavigate();
+
+  const handleSection = (path) => {
+    setCurrentSection(path);
+    navigate(path);
+  };
+
+  return (
+    <ul className={styles.list}>
+      {sidebarData.slice(0, 7).map((item, i) => (
+        <li
+          key={item.path || i}
+          className={`${styles.item} ${
+            currentSection === item.path ? styles.active : ""
+          }`}
+          onClick={() => handleSection(item.path)}
+        >
+          {item.icon}
+          {isCollapsed ? "" : item.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavList;
