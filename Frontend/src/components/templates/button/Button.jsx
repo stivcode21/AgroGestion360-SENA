ce5a9a353fb3676e5cb36a9c574f9d19206d74e0
stivcode21 <stivenelcodificador@gmail.com>
2025-12-01 21:42:54 -0500

    feat: now Now the button can receive a new type of button

--- Frontend/src/components/templates/button/Button.jsx
+++ Frontend/src/components/templates/button/Button.jsx
@@ -1,8 +1,9 @@
 import styles from "./Button.module.css";
 
 const Button = ({ children, type = "primary", ...props }) => {
-  const variant = type === "secondary" ? "secondary" : "primary";
-  const className = `${styles.button} ${styles[variant]}`;
+  const variant = ["secondary", "primary", "three"];
+  const current = variant.find((item) => item === type);
+  const className = `${styles.button} ${styles[current]}`;
 
   return (
     <button type="button" className={className} {...props}>
