7865128560a6e3b5c862e1696b728e35535eaba9
stivcode21 <stivenelcodificador@gmail.com>
2025-12-07 19:57:33 -0500

    feat: logic for collapsing the modal

--- Frontend/src/components/templates/detailsModal/DetailsModal.jsx
+++ Frontend/src/components/templates/detailsModal/DetailsModal.jsx
@@ -1,11 +1,13 @@
 import { X } from "lucide-react";
 import styles from "./DetailsModal.module.css";
+import { useModalStore } from "@/store/modalStore";
 
 const DetailsModal = ({ children }) => {
+  const { setIsOpenModal } = useModalStore();
   return (
-    <div className={styles.background}>
-      <div className={styles.modal}>
-        <button className={styles.btn}>
+    <div className={styles.background} onClick={() => setIsOpenModal(false)}>
+      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        <button className={styles.btn} onClick={() => setIsOpenModal(false)}>
           <X className={styles.close} />
         </button>
         {children}
