2009b0670d06aae76d343050272fdc549f942758
stivcode21 <stivenelcodificador@gmail.com>
2025-12-08 21:29:21 -0500

    update: The store has been updated

--- Frontend/src/store/modalStore.js
+++ Frontend/src/store/modalStore.js
@@ -3,6 +3,8 @@ import { create } from "zustand";
 export const useModalStore = create((set) => ({
   isOpenModal: false,
   selectProduct: null,
+  selectWoker: null,
   setSelectProduct: (product) => set({ selectProduct: product }),
+  setSelectWoker: (woker) => set({ selectWoker: woker }),
   setIsOpenModal: (isOpen) => set({ isOpenModal: isOpen }),
 }));
