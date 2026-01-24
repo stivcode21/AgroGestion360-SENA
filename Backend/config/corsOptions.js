26760f02402acdaae620f6807261ecd66faf6036
stivcode21 <stivenelcodificador@gmail.com>
2026-01-23 23:39:27 -0500

    feat: create corsConfig

---
+++ Backend/config/corsOptions.js
@@ -0,0 +1,13 @@
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://agrogestion.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
