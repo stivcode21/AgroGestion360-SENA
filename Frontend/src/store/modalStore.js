18a684cbb06c79640320721807535cde19650d20
stivcode21 <stivenelcodificador@gmail.com>
2026-01-29 22:08:15 -0500

    feat: New global states were created for state control of the new components

--- Frontend/src/store/modalStore.js
+++ Frontend/src/store/modalStore.js
@@ -4,7 +4,13 @@ export const useModalStore = create((set) => ({
   isOpenModal: false,
   selectProduct: null,
   selectWoker: null,
+  selectActivity: null,
+  selectCattle: null,
+  selectPig: null,
   setSelectProduct: (product) => set({ selectProduct: product }),
   setSelectWoker: (woker) => set({ selectWoker: woker }),
+  setSelectActivity: (activity) => set({ selectActivity: activity }),
+  setSelectCattle: (cattle) => set({ selectCattle: cattle }),
+  setSelectPig: (pig) => set({ selectPig: pig }),
   setIsOpenModal: (isOpen) => set({ isOpenModal: isOpen }),
 }));
