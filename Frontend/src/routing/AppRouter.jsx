0f3807d506cfd2d018203dae4612385958e1e5d7
stivcode21 <stivenelcodificador@gmail.com>
2026-02-20 21:28:57 -0500

    refactor: The entire structure of the component is modified to something much more organized.

--- Frontend/src/routing/AppRouter.jsx
+++ Frontend/src/routing/AppRouter.jsx
@@ -16,81 +16,88 @@ import ActivityForm from "@/pages/activityForm/ActivityForm";
 import GanaderiaForm from "@/pages/ganaderiaForm/GanaderiaForm";
 import PorciculturaForm from "@/pages/porciculturaForm/PorciculturaForm";
 import { checkAuth } from "../utils/auth";
+import Settings from "../pages/settings/Settings";
 
 const AppRouter = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  // useEffect(() => {
-  //   const verifySession = async () => {
-  //     const loggedIn = await checkAuth();
-  //     const publicRoutes = ["/", "/login"];
-  //     const isPublic = publicRoutes.includes(location.pathname);
 
-  //     if (!loggedIn) {
-  //       if (!isPublic) {
-  //         navigate("/login", { replace: true });
-  //       }
-  //       return;
-  //     }
-  
+  useEffect(() => {
+    const verifySession = async () => {
+      const loggedIn = await checkAuth();
+      const publicRoutes = ["/", "/login"];
+      const isPublic = publicRoutes.includes(location.pathname);
 
-  //     if (isPublic) {
-  //       navigate("/dashboard", { replace: true });
-  //     }
-  //   };
-  //   verifySession();
-  // }, [location.pathname, navigate]);
+      if (!loggedIn) {
+        if (!isPublic) {
+          navigate("/login", { replace: true });
+        }
+        return;
+      }
+
+      if (isPublic) {
+        navigate("/dashboard", { replace: true });
+      }
+    };
+    verifySession();
+  }, [location.pathname, navigate]);
 
   return (
     <Routes>
+      {/*  rutas de navegacion  */}
       <Route path="/" element={<Welcome />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/settings" element={<Settings />} />
       <Route path="/inventario" element={<Inventario />} />
-      <Route
-        path="/inventario/registrar"
-        element={<ProductForm title="Registrar producto" />}
-      />
-      <Route
-        path="/inventario/editar/:id"
-        element={<ProductForm title="Editar Producto" />}
-      />
       <Route path="/reportes" element={<Reportes />} />
       <Route path="/trabajadores" element={<Workers />} />
-      <Route
-        path="/trabajadores/registrar"
-        element={<WorkerForm title="Registrar trabajador" />}
-      />
-      <Route
-        path="/trabajadores/editar/:id"
-        element={<WorkerForm title="Editar trabajador" />}
-      />
       <Route path="/actividades" element={<Actividades />} />
+      <Route path="/ganaderia" element={<Ganaderia />} />
+      <Route path="/porcicultura" element={<Porcicultura />} />
+
+      {/*  subrutas para crear nuevos registros  */}
       <Route
         path="/actividades/registrar"
         element={<ActivityForm title="Registrar actividad" />}
       />
       <Route
-        path="/actividades/editar/:id"
-        element={<ActivityForm title="Editar actividad" />}
+        path="/trabajadores/registrar"
+        element={<WorkerForm title="Registrar trabajador" />}
       />
-      <Route path="/ganaderia" element={<Ganaderia />} />
       <Route
         path="/ganaderia/registrar"
         element={<GanaderiaForm title="Registrar animal" />}
       />
       <Route
-        path="/ganaderia/editar/:id"
-        element={<GanaderiaForm title="Editar animal" />}
+        path="/inventario/registrar"
+        element={<ProductForm title="Registrar producto" />}
       />
-      <Route path="/porcicultura" element={<Porcicultura />} />
       <Route
         path="/porcicultura/registrar"
         element={<PorciculturaForm title="Registrar porcino" />}
       />
+
+      {/* rutas dinamicas el id se obtiene de la url */}
+      <Route
+        path="/trabajadores/editar/:id"
+        element={<WorkerForm title="Editar trabajador" />}
+      />
+      <Route
+        path="/actividades/editar/:id"
+        element={<ActivityForm title="Editar actividad" />}
+      />
+      <Route
+        path="/ganaderia/editar/:id"
+        element={<GanaderiaForm title="Editar animal" />}
+      />
       <Route
         path="/porcicultura/editar/:id"
         element={<PorciculturaForm title="Editar porcino" />}
+      />
+      <Route
+        path="/inventario/editar/:id"
+        element={<ProductForm title="Editar Producto" />}
       />
     </Routes>
   );
