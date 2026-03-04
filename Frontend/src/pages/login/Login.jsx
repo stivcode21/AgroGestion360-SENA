3d7c7e2eceba878c592c9f25ff926285894c6c3a
sebas <sebastianzapata503@gmail.com>
2026-03-04 13:34:12 +0100

    feat: Forgot your password?

--- Frontend/src/pages/login/Login.jsx
+++ Frontend/src/pages/login/Login.jsx
@@ -114,6 +114,9 @@ const Login = () => {
           iniciar sesión
           <ArrowRight />
         </Button>
+        <a href="/aaaaa" className={styles.link}>
+          ¿olvidaste tu contraseña?
+        </a>
       </form>
       <footer className={styles.footer}>
         Accede como dueño o administrador según tus credenciales
