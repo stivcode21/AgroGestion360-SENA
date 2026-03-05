6105d86b816e76ddc648763332c83eac1b3a2f40
stivcode21 <stivenelcodificador@gmail.com>
2026-03-04 21:41:40 -0500

    refactor: restructure database schema for agro_db with new tables and relationships

--- db/agroGestion.sql
+++ db/agroGestion.sql
@@ -1,18 +1,18 @@
-CREATE DATABASE ganaderia_db;
-USE ganaderia_db;
+CREATE DATABASE agro_db;
+USE agro_db;
 
-CREATE TABLE categoria (
-    id_categoria INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(45) NOT NULL
-);
-
-CREATE TABLE estados_actividades (
-    id_estado INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE tipo_insumo (
+    id_tipo INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE estado_pago (
     id_estado_pago INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE tipo_documento (
+    id_tipo INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL
 );
 
@@ -26,140 +26,163 @@ CREATE TABLE tipo_trabajador (
     nombre VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE tipo_animal (
-    id_tip_animal INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(60) NOT NULL
+CREATE TABLE tipo_ganado (
+    id_tipo INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE vacuna (
-    id_vacuna INT PRIMARY KEY AUTO_INCREMENT,
-    tipo_vacuna VARCHAR(45) NOT NULL
+CREATE TABLE tipo_origen (
+    id_tipo INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE tipo_reporte (
-    id_tipo_reporte INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(45) NOT NULL
+CREATE TABLE estado_animal (
+    id_estado INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE estado_actividades (
+    id_estado INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE usuarios (
     id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50) NOT NULL,
+    nombre_completo VARCHAR(50) NOT NULL,
+    edad INT,
+    id_tipo_documento INT NOT NULL,
+    numero_documento VARCHAR(20) NOT NULL, 
+    celular VARCHAR(20) NOT NULL,
     url_img VARCHAR(255),
-    usuario VARCHAR(45) NOT NULL,
-    contrasena VARCHAR(45) NOT NULL,
-    id_rol INT,
-    FOREIGN KEY (id_rol) REFERENCES roles(id_rol)
+    correo VARCHAR(45) NOT NULL,
+    contrasena VARCHAR(50) NOT NULL,
+    id_rol INT NOT NULL,
+    FOREIGN KEY (id_rol) REFERENCES roles(id_rol),
+    FOREIGN KEY (id_tipo_documento) REFERENCES tipo_documento(id_tipo)
 );
 
 CREATE TABLE trabajadores (
     id_trabajador INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_completo VARCHAR(50) NOT NULL,
+    id_tipo_documento INT NOT NULL,
+    numero_documento VARCHAR(20) NOT NULL,
+    celular VARCHAR(20) NOT NULL,
+    id_tipo_trabajador INT NOT NULL,
+    direccion VARCHAR(50),
+    observaciones VARCHAR(200),
+    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_tipo_trabajador) REFERENCES tipo_trabajador(id_tipo),
+    FOREIGN KEY (id_tipo_documento) REFERENCES tipo_documento(id_tipo)
+);
+
+CREATE TABLE inventario (
+    id_insumo INT PRIMARY KEY AUTO_INCREMENT,
+    id_tipo INT NOT NULL,
     nombre VARCHAR(50) NOT NULL,
-    celular INT,
-    estado INT,
-    id_tipo INT,
-    FOREIGN KEY (id_tipo) REFERENCES tipo_trabajador(id_tipo)
+    marca VARCHAR(50) ,
+    cantidad INT NOT NULL,
+    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fecha_vencimiento DATETIME,
+    unidad_medida VARCHAR(20),
+    proveedor VARCHAR(45),
+    precio_unitario INT NOT NULL,
+    observaciones VARCHAR(250),
+    FOREIGN KEY (id_tipo) REFERENCES tipo_insumo(id_tipo)
 );
 
-CREATE TABLE recursos (
-    id_recurso INT PRIMARY KEY AUTO_INCREMENT,
-    tipo VARCHAR(45) NOT NULL,
-    nombre VARCHAR(45) NOT NULL,
-    cantidad INT,
-    fecha_registro DATETIME,
-    unidad_medida VARCHAR(45),
-    precio_unitario INT,
-    descripcion VARCHAR(100)
-);
-
-CREATE TABLE animales (
+CREATE TABLE ganaderia (
     id_animal INT PRIMARY KEY AUTO_INCREMENT,
-    raza VARCHAR(45),
+    fecha_ingreso DATETIME DEFAULT CURRENT_TIMESTAMP,
+    id_tipo INT NOT NULL,
+    id_tipo_origen INT NOT NULL,
     fecha_nacimiento DATETIME,
-    fecha_ingreso DATETIME,
-    tipo_origen VARCHAR(45),
-    peso_inicial VARCHAR(45),
-    peso_actual VARCHAR(45),
-    estado VARCHAR(45),
-    observaciones VARCHAR(255),
-    id_tip_animal INT,
-    FOREIGN KEY (id_tip_animal) REFERENCES tipo_animal(id_tip_animal)
+    peso_inicial DECIMAL(10,2) NOT NULL,
+    estado_salud VARCHAR(45),
+    id_estado INT NOT NULL,
+    observaciones VARCHAR(250),
+    marcado BOOLEAN,
+    FOREIGN KEY (id_tipo_origen) REFERENCES tipo_origen(id_tipo),
+    FOREIGN KEY (id_tipo) REFERENCES tipo_ganado(id_tipo),
+    FOREIGN KEY (id_estado) REFERENCES estado_animal(id_estado)
 );  
+
+CREATE TABLE porcicultura (
+    id_animal INT PRIMARY KEY AUTO_INCREMENT,
+    fecha_ingreso DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fecha_nacimiento DATETIME,
+    peso_inicial DECIMAL(10,2) NOT NULL,
+    sexo VARCHAR(45) NOT NULL,
+    estado_salud VARCHAR(45),
+    id_estado INT NOT NULL,
+    observaciones VARCHAR(250),
+    FOREIGN KEY (id_estado) REFERENCES estado_animal(id_estado)
+);  
+
+CREATE TABLE vacunas (
+    id_registro INT PRIMARY KEY AUTO_INCREMENT,
+    tipo_vacuna VARCHAR(50),
+    fecha_aplicacion DATETIME DEFAULT CURRENT_TIMESTAMP,
+    dosis VARCHAR(45) NOT NULL,
+    responsable VARCHAR(60) NOT NULL,
+    id_animal INT NOT NULL,
+    tipo_animal VARCHAR(45),
+    estado_vacunacion BOOLEAN DEFAULT FALSE,
+    observaciones VARCHAR(250)
+);
 
 CREATE TABLE ventas_animales (
     id_venta INT PRIMARY KEY AUTO_INCREMENT,
+    id_animal INT NOT NULL,
+    tipo_animal VARCHAR(45),
     comprador VARCHAR(45),
-    fecha DATETIME,
-    monto_total INT,
-    observaciones VARCHAR(45),
-    id_animal INT,
-    FOREIGN KEY (id_animal) REFERENCES animales(id_animal)
+    fecha_venta DATETIME DEFAULT CURRENT_TIMESTAMP,
+    monto_total DECIMAL(10,2) NOT NULL,
+    observaciones VARCHAR(250)
 );
 
 CREATE TABLE consumo_insumo (
-    id_egreso INT PRIMARY KEY AUTO_INCREMENT,
-    descripcion VARCHAR(100),
-    cantidad INT,
-    fecha_registro DATETIME,
-    id_recurso INT,
-    FOREIGN KEY (id_recurso) REFERENCES recursos(id_recurso)
-);
-
-CREATE TABLE reportes (
-    id_reportes INT PRIMARY KEY AUTO_INCREMENT,
-    fecha DATETIME,
-    descripcion VARCHAR(45),
-    id_tipo_reporte INT,
-    FOREIGN KEY (id_tipo_reporte) REFERENCES tipo_reporte(id_tipo_reporte)
+    id_consumo  INT PRIMARY KEY AUTO_INCREMENT,
+    actividad_asociada VARCHAR(50),
+    cantidad INT NOT NULL,
+    unidad_medida VARCHAR(20),
+    id_responsable INT NOT NULL,
+    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    id_insumo INT NOT NULL,
+    FOREIGN KEY (id_insumo) REFERENCES inventario(id_insumo),
+    FOREIGN KEY (id_responsable) REFERENCES trabajadores(id_trabajador)
 );
 
 CREATE TABLE actividades (
     id_registro INT PRIMARY KEY AUTO_INCREMENT,
-    url_evidencia VARCHAR(50),
-    monto DOUBLE,
-    descripcion TEXT,
-    fecha_inicio DATETIME,
+    url_evidencia VARCHAR(250),
+    fecha_inicio DATETIME DEFAULT CURRENT_TIMESTAMP,
     fecha_fin DATETIME,
-    fecha_creacion DATETIME,
-    observacion VARCHAR(100),
-    categoria_id_categoria INT,
-    estados_id_estado INT,
-    FOREIGN KEY (categoria_id_categoria) REFERENCES categoria(id_categoria),
-    FOREIGN KEY (estados_id_estado) REFERENCES estados_actividades(id_estado)
+    duracion VARCHAR(30),
+    id_trabajador INT NOT NULL,
+    id_estado INT NOT NULL,
+    monto DECIMAL(10,2) NOT NULL,
+    observaciones VARCHAR(250),
+    FOREIGN KEY (id_estado) REFERENCES estado_actividades(id_estado),
+    FOREIGN KEY (id_trabajador) REFERENCES trabajadores(id_trabajador)
 );
 
-CREATE TABLE actividades_has_trabajador (
-    actividades_id_registro INT,
-    trabajadores_id_trabajador INT,
-    detalle_pago INT,
-    PRIMARY KEY (actividades_id_registro, trabajadores_id_trabajador),
-    FOREIGN KEY (actividades_id_registro) REFERENCES actividades(id_registro),
-    FOREIGN KEY (trabajadores_id_trabajador) REFERENCES trabajadores(id_trabajador)
-);
-
-CREATE TABLE detalle_pago (
-    id_pago INT,
-    actividades_has_trabajador_id_act_trab INT,
-    PRIMARY KEY (id_pago, actividades_has_trabajador_id_act_trab)
+CREATE TABLE solicitud (
+    id_solicitud INT PRIMARY KEY AUTO_INCREMENT,
+    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    titulo VARCHAR(50) NOT NULL,
+    motivo VARCHAR(100) NOT NULL,
+    id_tipo_insumo INT NOT NULL,
+    cantidad INT NOT NULL,
+    especie_destino VARCHAR(30),
+    unidad_medida VARCHAR(20),
+    proveedor VARCHAR(30),
+    fecha_vencimiento DATETIME,
+    FOREIGN KEY (id_tipo_insumo) REFERENCES tipo_insumo(id_tipo)
 );
 
 CREATE TABLE Pago (
     id_pago INT PRIMARY KEY AUTO_INCREMENT,
-    fecha_pago DATETIME,
-    concepto VARCHAR(45),
-    monto DOUBLE,
-    estado_pago_id_estado_pago INT,
-    FOREIGN KEY (estado_pago_id_estado_pago) REFERENCES estado_pago(id_estado_pago)
-);
-
-CREATE TABLE animales_has_vacuna (
-    animales_id_animal INT,
-    fecha_aplicacion DATETIME,
-    dosis_aplicada VARCHAR(45),
-    responsable_aplicacion VARCHAR(45),
-    estado_vacunacion VARCHAR(45),
-    observacion VARCHAR(45),
-    vacuna_id_vacuna INT,
-    PRIMARY KEY (animales_id_animal, vacuna_id_vacuna),
-    FOREIGN KEY (animales_id_animal) REFERENCES animales(id_animal),
-    FOREIGN KEY (vacuna_id_vacuna) REFERENCES vacuna(id_vacuna)
+    fecha_pago DATETIME DEFAULT CURRENT_TIMESTAMP,
+    id_actividad INT,
+    FOREIGN KEY (id_actividad) REFERENCES actividades(id_registro)
 );
