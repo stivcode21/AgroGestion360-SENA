18fe3b1442fee468c3c8057f16c61555a32be121
stivcode21 <stivenelcodificador@gmail.com>
2026-03-08 18:14:28 -0500

    feat: new request in TableLayout component with pagination and error handling

--- Frontend/src/components/templates/tableLayout/TableLayout.jsx
+++ Frontend/src/components/templates/tableLayout/TableLayout.jsx
@@ -1,10 +1,51 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import styles from "./TableLayout.module.css";
+import toast from "react-hot-toast";
+import { useLoader } from "@/context/loaderProvider/LoaderProvider";
+import { buildApiUrl } from "@/utils/apiBase";
 
-const TableLayout = ({ headers, columns, compactColumns, children }) => {
+const TableLayout = ({
+  headers,
+  columns,
+  compactColumns,
+  children,
+  page,
+  setPage,
+  setProducts,
+  endpoint,
+}) => {
   const resolvedHeaders = headers ?? [];
   const resolvedColumns = columns;
   const resolvedCompactColumns = compactColumns ?? columns;
+
+  const { toggleLoader } = useLoader();
+
+  const changePage = async (isNextPage) => {
+    const nextPage = isNextPage ? page + 1 : page - 1;
+    if (nextPage < 1) return;
+
+    try {
+      toggleLoader(true);
+      const res = await fetch(buildApiUrl(`${endpoint}/${nextPage}`), {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message);
+        return;
+      }
+
+      setProducts(data.data);
+      setPage(nextPage);
+    } catch (error) {
+      console.error("Error en inicio de sesion:", error);
+      toast.error("Ha ocurrido un error inesperado.");
+    } finally {
+      toggleLoader(false);
+    }
+  };
 
   return (
     <div
@@ -23,12 +64,21 @@ const TableLayout = ({ headers, columns, compactColumns, children }) => {
       <ul className={styles.tableBody}>{children}</ul>
 
       <footer className={styles.tableFooter}>
-        <span>Pagina 1</span>
+        <span>{`Pagina ${page}`}</span>
         <div className={styles.pagination}>
-          <button type="button" className={styles.actionButton} disabled>
+          <button
+            type="button"
+            className={styles.actionButton}
+            disabled={page <= 1}
+            onClick={() => changePage(false)}
+          >
             <ChevronLeft />
           </button>
-          <button type="button" className={styles.actionButton}>
+          <button
+            type="button"
+            onClick={() => changePage(true)}
+            className={styles.actionButton}
+          >
             <ChevronRight />
           </button>
         </div>
