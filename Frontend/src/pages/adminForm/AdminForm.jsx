e58d469b89de9032facfe414f33443c4755e93be
stivcode21 <stivenelcodificador@gmail.com>
2026-03-08 18:11:09 -0500

    feat: add password input with visibility toggle and update styles for better layout

--- Frontend/src/pages/adminForm/AdminForm.jsx
+++ Frontend/src/pages/adminForm/AdminForm.jsx
@@ -1,18 +1,23 @@
 import MainLayout from "@/components/templates/mainLayout/MainLayout";
 import styles from "./AdminForm.module.css";
 import Button from "@/components/templates/button/Button";
-import { ArrowLeft, Save } from "lucide-react";
+import { ArrowLeft, Eye, EyeClosed, Save } from "lucide-react";
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import FormInput from "@/components/molecules/formInput/FormInput";
 import ImagePicker from "@/components/atoms/imagePicker/ImagePicker";
 import { adminsData } from "@/data/adminsData";
-import { adminFieldValidations, adminInputFields } from "@/data/adminRegisterData";
+import {
+  adminFieldValidations,
+  adminInputFields,
+} from "@/data/adminRegisterData";
 
 const AdminForm = ({ title }) => {
   const inputRef = useRef(null);
   const [previewUrl, setPreviewUrl] = useState("");
   const [errors, setErrors] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
 
   const { id } = useParams();
   const isEditMode = Boolean(id);
@@ -95,9 +100,26 @@ const AdminForm = ({ title }) => {
                   error={errors[field.name]}
                   onBlur={handleBlur}
                   type={field?.type}
-                  defaultValue={isEditMode ? admin?.[field.name] ?? "" : ""}
+                  defaultValue={isEditMode ? (admin?.[field.name] ?? "") : ""}
                 />
               ))}
+            </div>
+            <div className={styles.containerInput}>
+              <span className={styles.label}>Contraseña:</span>
+              <input
+                type={showPassword ? "text" : "password"}
+                className={styles.input}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className={styles.eyeButton}
+              >
+                {showPassword ? <Eye /> : <EyeClosed />}
+              </button>
             </div>
             <div className={styles.footerActions}>
               <Button type="three">
