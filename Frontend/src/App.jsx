c1669fc4dd35a0bc5e3fa5df77115ef384a2d290
Gustavo Olarte <149637502+DevProCodeLab@users.noreply.github.com>
2026-03-09 22:28:55 -0500

    feat: import action modal provider

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routing/AppRouter";
 import { LoaderProvider } from "@/context/loaderProvider/LoaderProvider";
 import { Toaster } from "react-hot-toast";
+import { ActionModalProvider } from "@/context/actionModalProvider/ActionModalProvider";
 
 function App() {
   return (
     <BrowserRouter>
       <LoaderProvider>
-        <Toaster position="top-center" />
-        <AppRouter />
+        <ActionModalProvider>
+          <Toaster position="top-center" />
+          <AppRouter />
+        </ActionModalProvider>
       </LoaderProvider>
     </BrowserRouter>
   );
