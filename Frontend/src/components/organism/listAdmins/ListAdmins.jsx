cc13b7dfefd612030ba8d07e328fc35747c622bb
Gustavo Olarte <149637502+DevProCodeLab@users.noreply.github.com>
2026-03-16 13:03:19 -0500

    feat: This component now retrieves data from the database to map the administrator list

--- Frontend/src/components/organism/listAdmins/ListAdmins.jsx
+++ Frontend/src/components/organism/listAdmins/ListAdmins.jsx
@@ -1,11 +1,43 @@
 import styles from "./ListAdmins.module.css";
 import AdminCard from "@/components/molecules/adminCard/AdminCard";
-import { adminsData } from "@/data/adminsData";
 import { Link } from "react-router-dom";
 import Button from "@/components/templates/button/Button";
 import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useLoader } from "@/context/loaderProvider/LoaderProvider";
+import { buildApiUrl } from "@/utils/apiBase";
 
-const ListAdmins = ({ onOpenCredentials }) => {
+const ListAdmins = () => {
+  const [admins, setAdmins] = useState();
+  const { toggleLoader } = useLoader();
+
+  useEffect(() => {
+    const getAdmins = async () => {
+      try {
+        toggleLoader(true);
+        const res = await fetch(buildApiUrl("auth/admins/list"), {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+          toast.error(data.message);
+          return;
+        }
+
+        setAdmins(data.admins);
+      } catch (error) {
+        console.error("Error en getProducts:", error);
+        toast.error("Ha ocurrido un error inesperado.");
+      } finally {
+        toggleLoader(false);
+      }
+    };
+    getAdmins();
+  }, []);
+
   return (
     <section className={styles.container}>
       <header className={styles.header}>
@@ -25,12 +57,8 @@ const ListAdmins = ({ onOpenCredentials }) => {
       </header>
 
       <section className={styles.grid}>
-        {adminsData.map((admin) => (
-          <AdminCard
-            key={admin.id}
-            admin={admin}
-            onOpenCredentials={onOpenCredentials}
-          />
+        {admins?.map((admin) => (
+          <AdminCard key={admin.id_usuario} admin={admin} />
         ))}
       </section>
     </section>
