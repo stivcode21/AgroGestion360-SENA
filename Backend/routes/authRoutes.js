24f118575858ecff2fc6949bd31672b8981af0b5
Gustavo Olarte <149637502+DevProCodeLab@users.noreply.github.com>
2026-03-16 12:56:11 -0500

    feat: integrate new features with router for be consulted

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -5,10 +5,16 @@ const { verifyToken } = require("../middleware/authMiddleware.js");
 const router = express.Router();
 
 router.post("/login", auth.loginController);
-router.post("/user", auth.userController);
 router.post("/logout", auth.logoutController);
+router.get("/user/:id", auth.userController); 
 router.get("/verify", verifyToken, (req, res) => {
   res.json({ message: "Acceso concedido", user: req.user });
 });
+
+router.get("/admins/list", auth.getAdmins);
+router.put("/update/:id", auth.editAdmin);
+router.post("/admin", auth.createAdminController);
+
+
 
 module.exports = router;
